d3653a8a10e01d12c6ba356ce0a776f1e4f68981
samdewar <72562265+samdewar@users.noreply.github.com>
2023-11-17 15:00:02 +0000

    Had an oopsie with gitignore

--- lib/credentials.dart
+++ lib/credentials.dart
@@ -1,1 +1,1 @@
-final String connection = 'mongodb+srv://samhdewar:AkHpq8oOvKRMcevU@cluster0.5upeiin.mongodb.net/';
+final String connection = 'mongodb+srv://samhdewar:AkHpq8oOvKRMcevU@cluster0.5upeiin.mongodb.net/'; \\This is invalid now, you scoundrel 
