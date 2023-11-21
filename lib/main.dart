88c542f60cbb534bb45f19f993d9a02293369a82
samdewar <samdewar@protonmail.com>
2023-11-21 02:30:37 +0000

    simple line graph

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'credentials.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: siteName,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  LineChartData sampleData1() {
+    return LineChartData(
+      gridData: FlGridData(show: false),
+      titlesData: FlTitlesData(show: false),
+      borderData: FlBorderData(
+        show: true,
+        border: Border.all(color: const Color(0xff37434d), width: 1),
+      ),
+      minX: 0,
+      maxX: 7,
+      minY: 0,
+      maxY: 6,
+      lineBarsData: [
+        LineChartBarData(
+          spots: [
+            FlSpot(0, 3),
+            FlSpot(1, 1),
+            FlSpot(2, 4),
+            FlSpot(3, 2),
+            FlSpot(4, 5),
+            FlSpot(5, 1),
+            FlSpot(6, 29),
+          ],
+          isCurved: false,
+          // colors: [Colors.blue],
+          dotData: FlDotData(show: false),
+          belowBarData: BarAreaData(show: false),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: TextButton(onPressed: null, child: Text("Hello World")),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(siteName),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: LineChart(
+          sampleData1(),
         ),
       ),
     );
